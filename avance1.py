6863f91a5136d20903809241f0441a60ea3ecbbc
SaMeSparts <diego.gonzalez4390@gmail.com>
2024-08-19 16:32:01 -0600

    agregre ya lo del gasto y lo del que el programa se repita hasta que el usa usuario quiera y necesito encontrar el porque se imprime saldo.txt

--- avance1.py
+++ avance1.py
@@ -1,19 +1,81 @@
 import math 
 import random
+import os
 
 
-nombre_empresa=input("¿Como se llama tu empresa?")
-dinero_cliente=0
-dinero_empresa=0
 
-print(f"Bienvenido, esta es la Empresa: {nombre_empresa}")
 
-materiales_estucturales={"Hormigon":80,"Acero":1,"Madera":400,"Ladrillo":0.30,"Piedra":50,"Aluminio":2}
-materiales_acabado={"Yeso":0.30,"Vidrio":20,"Tejas":0.50,"Pintura":10,"Baldosas":50,"Papel Pintado":4}
-materiales_aislantes={"Lana Mineral":80,"Espuma de poliuretano":10,"Poliestireno expandido":5,"Corcho:":15}
-materiales_unionyadhesivos={"Cemento":8,"Mortero":5,"Siliconas":4,"Resinas epoxi":20}
-materiales_instalaciones={"Cobre":8,"PVC":1,"Polipropileno":4,"Acero galvanizado":3,}
-materiales_naturales={"Madera":400,"Bambú":1,"Adobe":0.10,"Piedra":50}
-materiales_recubrimiento={"Enyesado":6,"Pladur":10,"Azulejos":25,"Mármol":100}
 
-print(materiales_acabado)
+saldo_cliente="saldocliente.txt"
+saldo_empresa="saldoempresa.txt"
+cantidad_pago_total=0
+repeticion=0
+
+def leer_saldo_cliente():
+    if os.path.exists(saldo_cliente):
+        with open(saldo_cliente, "r") as archivo_saldo_cliente:
+            return float(archivo_saldo_cliente.read().strip())
+    else: return 0.0
+def leer_saldo_empresa():
+    if os.path.exists(saldo_empresa):
+        with open(saldo_empresa, "r") as archivo_saldo_empresa:
+            return float(archivo_saldo_empresa.read().strip())
+    else: return 0.0
+def escribir_saldo_cliente(saldo):
+    with open(saldo_cliente, "w") as archivo_saldo_cliente:
+        archivo_saldo_cliente.write(str(saldo))
+def escribir_saldo_empresa(saldo2):
+    with open(saldo_empresa, "w") as archivo_saldo_empresa:
+        archivo_saldo_empresa.write(str(saldo2))
+def agregar_saldo_cliente(cantidad):
+    saldo = leer_saldo_cliente()
+    saldo += cantidad
+    escribir_saldo_cliente(saldo)
+    print(f"Se han agregado {cantidad:.2f} unidades. Saldo actual: {saldo:.2f}")
+def agregar_saldo_empresa(cantidad2):
+    saldo2 = leer_saldo_empresa()
+    saldo2 += cantidad2
+    escribir_saldo_empresa(saldo2)
+    print(f"Se han agregado {cantidad2:.2f} unidades. Saldo actual: {saldo2:.2f}")
+def pago_de_compra(cantidad):
+    saldo = leer_saldo_cliente()
+    if cantidad > saldo:
+        print(f"No hay suficiente saldo. Saldo actual: {saldo:.2f}, intentaste gastar {cantidad:.2f}")
+    else:
+        saldo -= cantidad
+        escribir_saldo_cliente(saldo)
+        print(f"Se han gastado {cantidad:.2f} unidades. Saldo actual: {saldo:.2f}, Gracias por su compra.")
+
+while repeticion == 0:
+
+    nombre_empresa=input("¿Como se llama tu empresa?\n")
+    añadir_dinero_cliente=float(input("Cuanto dinero vas a depositar:\n"))
+    agregar_saldo_cliente(añadir_dinero_cliente)
+    print(saldo_cliente)
+
+
+    print(f"Bienvenido, esta es la Empresa: {nombre_empresa}")
+
+    materiales_estucturales={"Hormigon":80,"Acero":1,"Madera":400,"Ladrillo":0.30,"Piedra":50,"Aluminio":2}
+    materiales_acabado={"Yeso":0.30,"Vidrio":20,"Tejas":0.50,"Pintura":10,"Baldosas":50,"Papel Pintado":4}
+    materiales_aislantes={"Lana Mineral":80,"Espuma de poliuretano":10,"Poliestireno expandido":5,"Corcho:":15}
+    materiales_unionyadhesivos={"Cemento":8,"Mortero":5,"Siliconas":4,"Resinas epoxi":20}
+    materiales_instalaciones={"Cobre":8,"PVC":1,"Polipropileno":4,"Acero galvanizado":3,}
+    materiales_naturales={"Madera":400,"Bambú":1,"Adobe":0.10,"Piedra":50}
+    materiales_recubrimiento={"Enyesado":6,"Pladur":10,"Azulejos":25,"Mármol":100}
+
+
+    print(f"Esta es la cantidad a pagar:{cantidad_pago_total}")
+    accion_pago=str(input("La desea pagar? (Si)(No):\n"))
+    if accion_pago.lower() == "si":
+        pago_de_compra(cantidad_pago_total)
+    else:
+        print("El pago ha sido cancelado")
+
+    apagar_programa=str(input("Desea terminar el programa (Si)(No)\n"))
+    if apagar_programa.lower() == "no":
+        repeticion += 1
+        print("Se finalizo el programa")
+    else:
+        print(".")
+
--- pruebas.py
+++ pruebas.py
@@ -30,6 +30,18 @@ def agregar_saldo_empresa(cantidad2):
     saldo2 += cantidad2
     escribir_saldo_empresa(saldo2)
     print(f"Se han agregado {cantidad2:.2f} unidades. Saldo actual: {saldo2:.2f}")
+def pago_de_compra(cantidad):
+    saldo = leer_saldo_cliente()
+    if cantidad > saldo:
+        print(f"No hay suficiente saldo. Saldo actual: {saldo:.2f}, intentaste gastar {cantidad:.2f}")
+    else:
+        saldo -= cantidad
+        escribir_saldo_cliente(saldo)
+        print(f"Se han gastado {cantidad:.2f} unidades. Saldo actual: {saldo:.2f}")
+
+
 
 añadir_dinero_cliente=float(input("Cuanto dinero vas a depositar:\n"))
 agregar_saldo_cliente(añadir_dinero_cliente)
+suma_dinero_cliente=float(input("Esta es la cantidad a pagar: \n"))
+pago_de_compra(suma_dinero_cliente)
